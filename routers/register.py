a19c1e3dd32a4d40a1bb83e3709b932c06460d82
mdang2905 <mdang2905@sdsu.edu>
2026-01-12 16:35:14 -0800

    fixed view_cart

--- routers/register.py
+++ routers/register.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
 from datetime import datetime
 from pydantic import BaseModel
-from models import Student, StudentClass, CurrentClasses
+from models import Student, StudentClass, CurrentClasses, Family, Classes
 from .auth import db_dependency, family_dependency
 from sqlalchemy import func, case
 
@@ -66,7 +67,7 @@ async def read_classes_by_category(category_order: list, student_id: int, db: Se
 
     return final_data
 
-
+# From select_classes.php lines 69-76
 @router.get("/{student_id}/read_current_LC_classes")
 async def read_current_LC_classes(student_id: int, db: db_dependency, family: family_dependency):
     student = db.query(Student).filter(Student.student_id == student_id, Student.family_id == family.get('family_id')).first()
@@ -75,7 +76,7 @@ async def read_current_LC_classes(student_id: int, db: db_dependency, family: fa
     return await read_classes_by_category(category_order, student_id, db)
     
     
-    
+# From select_classes2.php lines 82-88    
 @router.get("/{student_id}/read_current_EP_classes")
 async def read_current_EP_classes(student_id: int, db: db_dependency, family: family_dependency):
     student = db.query(Student).filter(Student.student_id == student_id, Student.family_id == family.get('family_id')).first()
@@ -108,18 +109,126 @@ async def select_classes(student_id: int, db: db_dependency, family: family_depe
     db.commit()
 
 
-@router.get("/checkout/{family_id}")
+# from checkout.php lines 106-114
+@router.get("/checkout")
 async def view_cart(db: db_dependency, family: family_dependency):
     current_year = datetime.now().year
+    cart = (db.query(Family.verified.label("verified"),
+                    Student.first_name.label("first_name"),
+                    Student.last_name.label("last_name"),
+                    Student.chinese_name.label("chinese_name"),
+                    StudentClass,  
+                    Classes.class_id.label("class_id"),
+                    Classes.title.label("title"),
+                    Classes.chinese_title.label("chinese_title"))
+        .select_from(Family)
+        .join(Student, Student.family_id == Family.family_id)
+        .join(StudentClass, and_(StudentClass.student_id == Student.student_id, StudentClass.paid == 0, StudentClass.wait == 0, StudentClass.year == current_year))
+        .join(Classes, Classes.class_id == StudentClass.class_id)
+        .filter(Family.family_id == family.get('family_id'))
+        .order_by(Student.dob, StudentClass.class_id)
+    )
 
-    student_ids = [
-        s.student_id
-        for s in db.query(Student.student_id)
-                   .filter(Student.family_id == family.get('family_id'))
-                   .all()
-    ]
+    results = cart.all()
+
+    final_data = []
+
+    for (
+        verified,
+        first_name,
+        last_name,
+        chinese_name,
+        sc_obj,
+        class_id,
+        title,
+        chinese_title,
+    ) in results:
+        item = {
+            c.name: getattr(sc_obj, c.name)
+            for c in sc_obj.__table__.columns
+        }
+        item.update({
+            "verified": verified,
+            "first_name": first_name,
+            "last_name": last_name,
+            "chinese_name": chinese_name,
+            "class_id": class_id,
+            "title": title,
+            "chinese_title": chinese_title,
+        })
+
+        final_data.append(item)
+
+    return final_data
+
     
-    return db.query(StudentClass).filter(StudentClass.student_id.in_(student_ids),
-                                        StudentClass.paid == False,
-                                        StudentClass.wait == False,
-                                        StudentClass.year == current_year).all()
+
+    
+    
+    
+
+
+
+
+""" from checkout.php 53-72, don't exactly know what this does
+current_year = datetime.now().year
+    cart_T1 = (
+        db.query(
+            Student.first_name.label("first_name"),
+            Student.last_name.label("last_name"),
+            Student.chinese_name.label("chinese_name"),
+            Student.student_id.label("student_id"),
+            Classes.class_id.label("class_id"),
+        )
+        .select_from(Family)
+        .join(Student, Student.family_id == Family.family_id)
+        .join(StudentClass, and_(StudentClass.student_id == Student.student_id, StudentClass.wait == 0, StudentClass.year == current_year))
+        .join( Classes, and_(StudentClass.class_id == Classes.class_id, Classes.class_id == 75))
+        .filter(StudentClass.paid == 0)
+        .filter(Family.family_id == family.get("family_id"))
+        .subquery()
+    )
+    codes = ['01','02A','02B','07A','07B','08','09A','09B','10B','14','15','16','17','18','19','BOOK']
+    cart_T2 = (
+        db.query(Student.first_name.label("first_name"),
+                Student.last_name.label("last_name"),
+                Student.chinese_name.label("chinese_name"),
+                Student.student_id.label("student_id"),
+                Classes.class_id.label("class_id"))
+        .select_from(Family)
+        .join(Student, Student.family_id == Family.family_id)
+        .join(StudentClass, and_(StudentClass.student_id == Student.student_id, StudentClass.wait == 0, StudentClass.year == current_year))
+        .join(Classes, and_(StudentClass.class_id == Classes.class_id, Classes.class_code.in_(codes)))
+        .filter(StudentClass.paid == 0)
+        .filter(Family.family_id == family.get('family_id'))
+        .subquery()
+    )
+
+    query = (
+    db.query(
+        cart_T1.c.first_name,
+        cart_T1.c.last_name,
+        cart_T1.c.chinese_name,
+        cart_T1.c.student_id,
+        cart_T1.c.class_id,
+    )
+    .outerjoin(cart_T2, cart_T1.c.student_id == cart_T2.c.student_id)
+    .filter(cart_T2.c.class_id.is_(None))
+    )
+
+    results = query.all()
+
+    if results is None:
+        raise HTTPException(status_code=204, detail='Cart empty')
+
+    return [
+        {
+            "first_name": r[0],
+            "last_name": r[1],
+            "chinese_name": r[2],
+            "student_id": r[3],
+            "class_id": r[4],
+        }
+        for r in results
+    ]
+"""
